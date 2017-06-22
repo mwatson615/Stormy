29e83f7a70abb42497ba1b0c905854badc780648
Michael Watson <m.watson.tn@gmail.com>
2017-06-22 00:40:59 -0500

    added async get request

--- app/src/main/java/com/mw/stormy/MainActivity.java
+++ app/src/main/java/com/mw/stormy/MainActivity.java
@@ -2,12 +2,53 @@ package com.mw.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "a1392459f432e48f2b7c913f4d57758f";
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+        String forecastUrl = "https://api.darksky.net/forecast/" +
+            apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(forecastUrl).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    if (response.isSuccessful()) {
+                        Log.v(TAG, response.body().string());
+                    }
+                } catch (IOException e) {
+                    Log.e(TAG, "Exception caught: ", e);
+                }
+            }
+        });
+        Log.d(TAG, "Main UI code is running!");
+
     }
 }
