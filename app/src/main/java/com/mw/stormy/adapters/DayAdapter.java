ad061149bcd0aa098dc1419eca15d95192e33ac7
Michael Watson <m.watson.tn@gmail.com>
2017-08-26 19:16:02 -0500

    updated adapter getview and viewholder

--- app/src/main/java/com/mw/stormy/adapters/DayAdapter.java
+++ app/src/main/java/com/mw/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package com.mw.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.mw.stormy.R;
 import com.mw.stormy.weather.Day;
 
 /**
@@ -39,6 +43,29 @@ public class DayAdapter extends BaseAdapter{
     //where the mapping occurs
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext)
+                    .inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView
+                    .findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView
+                    .findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView
+                    .findViewById(R.id.dayNameLabel);
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
         return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
     }
 }
